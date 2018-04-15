fd1f3567798b6fc75e2be48ef8ed64d9b4082ef2
Mattori <31841170+Mattori@users.noreply.github.com>
2018-04-15 19:35:10 +0200

    Ajout fonctionnel de l'inscription d'un développeur

--- app/components/developers/adding-developer.js
+++ app/components/developers/adding-developer.js
@@ -1,4 +1,81 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { isBlank } from '@ember/utils';
 
 export default Component.extend({
+  store: service(),
+  tagName: 'tr',
+
+  identity: undefined,
+  email: undefined,
+  login: undefined,
+  password: undefined,
+
+  init() {
+    this.initialize();
+    this._super();
+  },
+
+  initialize() {
+    this.devs().then((data) => {
+      this.set('owners', data);
+    });
+  },
+
+  devs() {
+    const store = this.get('store');
+    return store.findAll('developer');
+  },
+
+  actions: {
+    createDev() { // Création d'une ligne
+      let store = this.get('store');
+      let modelDev = 'developer';
+
+      let getIdentity = this.get('identity');
+      let getEmail = this.get('email');
+      let getLogin = this.get('login');
+      let getPassword = this.get('password');
+
+      let dataDev = {
+        'identity': getIdentity,
+        'email': getEmail,
+        'login': getLogin,
+        'password': getPassword,
+      };
+
+      if(!getIdentity
+      || !getEmail
+      || !getLogin
+      || !getPassword
+      ) {
+        console.log("Des données sont vides ! Échec de l'inscription !");
+      } else {
+        let newDev = store.createRecord(modelDev, dataDev);
+
+        newDev.save().then(() => {
+          console.log("Succès de l'inscription.");
+        });
+
+        this.set('identity', '');
+        this.set('email', '');
+        this.set('login', '');
+        this.set('password', '');
+      }
+    },
+
+    handleFocus(select, e) { // Ouverture du select en appuyant sur Tab lorsque l'on est sur l'input du code
+      if (this.focusFollowingInput(e)) {
+        select.actions.open();
+      }
+    }
+  },
+
+  focusFollowingInput(e) { // Focus/Focalisation sur le select suivant
+    let blurredElement = e.relatedTarget;
+    if (isBlank(blurredElement)) {
+      return false;
+    }
+    return !blurredElement.classList.contains('ember-power-select-search-input');
+  }
 });
--- app/models/developer.js
+++ app/models/developer.js
@@ -4,6 +4,7 @@ import {computed} from '@ember/object';
 export default DS.Model.extend({
   identity: DS.attr('string'),
   email: DS.attr('string'),
+  login: DS.attr('string'),
   password: DS.attr('string'),
   ownProject:DS.belongsTo('project'),
   projects: DS.hasMany('project',{inverse:'owner'}),
