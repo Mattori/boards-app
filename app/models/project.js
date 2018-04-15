d77a2329b223fe2704bdce8bd3dce97e2ceb2bf6
Mattori <31841170+Mattori@users.noreply.github.com>
2018-04-16 01:49:41 +0200

    Ajustement notifications requêtes

--- app/models/project.js
+++ app/models/project.js
@@ -2,13 +2,13 @@ import DS from 'ember-data';
 import {computed} from '@ember/object';
 
 export default DS.Model.extend({
-  name:DS.attr('string'),
-  description:DS.attr('string'),
-  startDate:DS.attr('date'),
-  dueDate:DS.attr('date'),
-  owner:DS.belongsTo('developer'),
-  developers:DS.hasMany('developer',{inverse:'ownProject'}),
-  stories:DS.hasMany('story'),
+  name: DS.attr('string'),
+  description: DS.attr('string'),
+  startDate: DS.attr('date'),
+  dueDate: DS.attr('date'),
+  owner: DS.belongsTo('developer'),
+  developers: DS.hasMany('developer',{inverse:'ownProject'}),
+  stories: DS.hasMany('story'),
   sDate: computed('startDate',function(){
     if(this.get('startDate'))
       return this.get('startDate').toLocaleDateString();
--- app/models/story.js
+++ app/models/story.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  code:DS.attr('string'),
-  description:DS.attr('string'),
+  code: DS.attr('string'),
+  description: DS.attr('string'),
   developer: DS.belongsTo('developer'),
   project: DS.belongsTo('project',{inverse:'stories'}),
   tags: DS.hasMany('tag')
