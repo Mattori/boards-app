58c0d947803413d7a36038f0f95793046327f23b
Mattori <31841170+Mattori@users.noreply.github.com>
2018-04-23 01:44:09 +0200

    Modif avec tentative de persistance d'affichage des projets d'un développeur après reload

--- app/routes/developer.js
+++ app/routes/developer.js
@@ -4,7 +4,7 @@ import RSVP from 'rsvp';
 export default Route.extend({
   model(params) {
     return RSVP.hash({
-      developer: this.get('store').findRecord('developer',params.developer_id,{include:"projects"})
+      developer: this.get('store').findRecord('developer', params.developer_id, {include:"projects"})
     });
   }
 });
